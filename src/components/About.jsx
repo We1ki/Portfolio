5c8e71cbe71d11f524c1146eec9b40ac10b4294f
Ibai <weikibai@gmail.com>
2025-12-06 13:58:55 +0100

    Started changing the about screen

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,16 @@
 export default function About() {
   return (
-    <section>
-      <h2>About Me</h2>
-
-      <p>
-        I'm a beginner developer learning React and building my first portfolio. I like combining design, animations and code to create smooth user experiences.
-      </p>
+    <section className="about-section">
+      <dic className="full-about">
+        <div className="about-text">
+          <h2>About Me</h2>
+          <p>
+            I'm a beginner developer learning React and building my first portfolio. I like combining design, animations and code to create smooth user experiences.
+          </p>
+        </div>
+        <img src="../../public/my-avatar.webp" alt="" />
+      </dic>
+      
     </section>
   );
 }
